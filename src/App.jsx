f165cf64840eab8a614f2156dffdab0bdad939ef
kaikunmidev <oluwakaikunmif@gmail.com>
2022-10-30 15:33:02 -0700

    i

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import react from 'react'
-import pic from "../src/assets/kai.png"
+import ava from "../src/assets/share.png"
+import ava2 from "../src/assets/share 2.png"
+import pic from "../src/assets/pic.png"
 import git from "../src/assets/git.png"
-import red from "../src/assets/reddot.png"
-import ingree from "../src/assets/ingree.png"
+import red from "../src/assets/dot.png"
+import ingree from "../src/assets/ingress.png"
+import slack from "../src/assets/slack.png"
+
 import './App.css'
 
 
@@ -11,29 +15,41 @@ function App() {
   return (
     <div className='container'>
       <div className="main_page">
-        <div className="picturepage">
-          <img src={pic} className="picture" alt=""  style={{height:"20vh"}}/>
-          <h4 className="name">Folorunsho kaikunmi</h4>
+        <div className="both">
+            <div className="sharre">
+      <img className='Share' src={ava} alt="" />      
+      </div>
+      <div className="sharee2">
+      <img className='Share2' src={ava2} alt="" />
+      </div>
+        </div>
+    
+        <div id= 'profile'>
+          <img src={pic} id='profile_img' alt=""  style={{height:"20vh"}}/>
+          <h4 id='twitter'>Folorunsho kaikunmi</h4>
+          <h5 id='slack'></h5>
         </div>
         <div className="links">
         <div className="input_link">
-          <button > <a href="https://twitter.com/thezuriteam">Twitter Link</a></button><br />
-          <button > <a href="https://www.linkedin.com/company/zuri-team/?originalSubdomain=ng">Zuri Team</a></button><br />
-          <button > <a href="https://www.nairaland.com/6704625/b-zuri-intenship-training-b">Zuri Books</a></button><br />
-          <button > <a href="https://medium.com/javarevisited/my-favorite-books-to-learn-python-in-depth-77465633b46e">Phyton Books</a></button><br />
-          <button > <a href="https://training.zuri.team/">Background check for coders</a></button><br />
-          <button > <a href="https://www.w3schools.com/">Design Books</a></button><br />
+          <button id='btn_twitter' > <a href="https://twitter.com/thezuriteam">Twitter Link</a></button><br />
+          <button id='btn_zuri' > <a href="https://books.zuri.team">Zuri Team</a></button><br />
+          <button id='books' > <a href="https://books.zuri.team">Zuri Books</a></button><br />
+          <button   id='book_phyton'> <a href="https://books.zuri.team">Phyton Books</a></button><br />
+          <button id='pitch' > <a href="https://background.zuri.team">Background check for coders</a></button><br />
+          <button id='book_design' > <a href="https://books.zuri.team/design-rules">Design Books</a></button><br />
+          
         </div>
         </div>
         <div className="footer1">
-        <img src={git} className="picture2" alt="" style={{height:"3vh"}}/>
+          <img className='picccccS' src={slack} alt="" />
+        <img src={git} className="picture2" alt="" />
         </div>
         <div className="footer2">
           <footer>
             <h5 className='zuri'>Zuri<img src={red} alt="" className='red' style={{height:"1vh"}}/>Internship</h5>
             <p className="front">HNG Internship 9 Frontend Task</p>
             <div className='ingggg'>
-            <img src={ingree} alt="" style={{height:"8vh"}} />
+            <img src={ingree} alt=""  />
             </div>
           </footer>
         </div>
